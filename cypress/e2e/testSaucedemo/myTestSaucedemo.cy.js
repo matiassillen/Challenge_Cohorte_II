19f6c8fa076b161642132ff868bd3649cc4c97f9
Matías Sillen Ríos <matias.sillen@gmail.com>
2024-12-11 17:08:40 -0300

    Add products to cart for user 1

--- cypress/e2e/testSaucedemo/myTestSaucedemo.cy.js
+++ cypress/e2e/testSaucedemo/myTestSaucedemo.cy.js
@@ -2,10 +2,24 @@ describe("testSaucedemo", { testIsolation: false }, () => {
   it("Buy with user 1", () => {
     cy.visit("https://www.saucedemo.com/");
 
-    //Login with the user "standard_user"
+    // Login with the user "standard_user"
 
     cy.get('input[placeholder="Username"]').type("standard_user");
     cy.get('input[placeholder="Password"]').type("secret_sauce");
     cy.get("#login-button").click();
+
+    // Add products to cart
+
+    // Verifica y hace clic en los elementos si existen
+    cy.get("body").then(($body) => {
+      if ($body.find(".btn.btn_primary.btn_small.btn_inventory").length) {
+        cy.get(".btn.btn_primary.btn_small.btn_inventory").each(($btn) => {
+          cy.wrap($btn).click();
+        });
+      } else {
+        cy.log("No se encontraron elementos con la clase especificada.");
+      }
+    });
+    cy.get('[data-test="shopping-cart-link"]').click();
   });
 });
